2e941ace77efd134d77a517ba925ab41e1092b4f
Daniel Willson <danielwillson004@gmail.com>
2025-10-02 19:21:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import os
 
 # Define the path to the train_images folder
-train_images_path = 'c:/Users/danie/Desktop/capstone project/train_images'
+train_images_path = 'C:\Users\danie\Desktop\capstone project\train_imagess'
 
 # Count the number of image files in the folder
 image_count = len([file for file in os.listdir(train_images_path) if os.path.isfile(os.path.join(train_images_path, file))])
